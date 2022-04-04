eac32932d2e4a55455b89d49d54202b9bd0eb3d2
akilinjamam <akilinjamam@gmail.comm>
2022-04-04 18:05:15 +0600

    reviews updating

--- src/Component/Reviews/Reviews.js
+++ src/Component/Reviews/Reviews.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import useReviews from '../../hooks/theReviews';
+import AllReviews from '../AllReviews/AllReviews';
 
 const Reviews = () => {
 
@@ -7,9 +8,21 @@ const Reviews = () => {
 
     return (
         <div>
-            <h2>this is reviews</h2>
+            <h2 className='text-center font-bold text-3xl'>All Reviews({reviews.length}) </h2>
+            <div>
+                {
+                    reviews.map(theReviews => <AllReviews
+                        key={theReviews.id}
+                        name={theReviews.name}
+                        image={theReviews.image}
+                        comment={theReviews.comment}
+                        ratings={theReviews.ratings}
+                    ></AllReviews>)
+                }
+            </div>
         </div>
     );
 };
 
-export default Reviews;
+
+export default Reviews
