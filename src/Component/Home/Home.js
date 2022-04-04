86bd9737c58c7ef399cb7bbe15c2cb40c04e8a4e
akilinjamam <akilinjamam@gmail.comm>
2022-04-04 15:14:21 +0600

    home-img-and-detail-part-half-done

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -10,28 +10,36 @@ const Home = () => {
             .then(data => setImage(data))
     }, [])
     return (
-        <div className='total-part'>
+        <div>
+            <div className='total-part'>
+                <div className="detail-part  ">
+                    <div className='w-4/6 m-auto mt-36 '>
+                        <h1 className='text-6xl font-bold'>
+                            Your Next Watch
+                        </h1>
 
-            <div className="detail-part  ">
-                <div className='w-4/6 m-auto mt-40 '>
-                    <h1 className='text-6xl font-bold'>
-                        Your Next Watch
-                    </h1>
-                    <br />
-                    <h1 className='text-6xl font-bold text-cyan-400'>
-                        Your Best Watch
-                    </h1>
+                        <h1 className='text-6xl font-bold text-cyan-400'>
+                            Your Best Watch
+                        </h1>
+                        <br />
+                        <p className='text-zinc-400'>A Wrist-Watch is a part of  personality for a men and woman. it glows your look and increase your confident. In our Website we have arranged lots of wrist-watches with lots of variation. we provide you with best quality according to your choice. to watch more. go to live demo.    </p> <br />
+
+                        <button className='bg-cyan-400 p-2 text-white font-bold text-2xl rounded hover:bg-cyan-600 duration-700 ease-out'>Live Demo</button>
+                    </div>
 
                 </div>
 
+
+                <div className="image-part">
+
+                    {
+                        image.map(theImg => <img src={theImg.image} alt="" />)
+                    }
+                </div>
             </div>
 
-
-            <div className="image-part">
-
-                {
-                    image.map(theImg => <img src={theImg.image} alt="" />)
-                }
+            <div className='text-center font-bold text-3xl'>
+                <h2>Customers Reviews()</h2>
             </div>
         </div>
     );
